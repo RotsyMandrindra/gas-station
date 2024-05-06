3d3074877e0953d0fc5a07a0bb6d3c866f30990f
RotsyMandrindra <rotsy.rasoanaivo@gmail.com>
2024-05-06 20:51:31 +0300

    feat : generate an empty spring project

---
+++ src/main/java/hei/school/gasstation/GasStationApplication.java
@@ -0,0 +1,13 @@
+package hei.school.gasstation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GasStationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GasStationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hei/school/gasstation/GasStationApplicationTests.java
@@ -0,0 +1,13 @@
+package hei.school.gasstation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GasStationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
