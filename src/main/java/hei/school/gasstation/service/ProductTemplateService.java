98464c030b62c18ec69afc9e9258c7ceffa28c39
RotsyMandrindra <rotsy.rasoanaivo@gmail.com>
2024-05-07 22:40:26 +0300

    feat : add service packege for all entity on the model package

---
+++ src/main/java/hei/school/gasstation/service/ProductTemplateService.java
@@ -0,0 +1,28 @@
+package hei.school.gasstation.service;
+
+import hei.school.gasstation.model.ProductTemplate;
+import hei.school.gasstation.repository.ProductTemplateCrudOperation;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.UUID;
+
+public class ProductTemplateService {
+    private ProductTemplateCrudOperation productTemplateCrudOperation;
+
+    public ProductTemplateService(ProductTemplateCrudOperation productTemplateCrudOperation) {
+        this.productTemplateCrudOperation = productTemplateCrudOperation;
+    }
+
+    public List<ProductTemplate> getAllProductTemplate() throws SQLException {
+        return productTemplateCrudOperation.findAll();
+    }
+    public ProductTemplate createProductTemplate(ProductTemplate toSave) throws SQLException {
+        return productTemplateCrudOperation.save(toSave);
+    }
+    public ProductTemplate updateProductTemplate(UUID id, ProductTemplate toUpdate) throws SQLException {
+        return productTemplateCrudOperation.update(id, toUpdate);
+    }
+    public void deleteProductTemplate(UUID id) throws SQLException {
+        productTemplateCrudOperation.delete(id);
+    }
+}
